aa8197f3d37c415bb68afcddc0af08be67db7726
zhjdtc <jz2678@columbia.edu>
2016-11-16 15:48:55 -0500

    Topic model on Lyric
    Select 20 topics based on lyrics and get the probability of distribution for prediction

---
+++ doc/topicmodel.R
@@ -0,0 +1,45 @@
+##topic model
+
+library(NLP)
+library(tm)
+library(lda)
+library(LDAvis)
+
+
+
+dat=lyr
+dat=dat[,-c(1,2,3,6:30)]   #clean lyrics data
+
+dat=t(t(dat))
+
+document.name=lyr[,1]
+document=vector("list",length(document.name))
+names(document)=document.name
+
+vocab=colnames(dat)
+for (i in 1:length(document)){
+  index=(dat[i,]!=0)
+  index=which(index=="TRUE")
+  document[[i]]=rbind(as.integer(index-1),as.integer(dat[i,index]))
+  
+}
+
+doc.length=sapply(document,function(x) sum(x[2,]))
+term.table=colSums(dat)
+term.frequency=as.integer(term.table)
+
+
+K=20
+G=5000
+alpha=0.02
+eta=0.02
+
+
+
+library(lda)
+fit=lda.collapsed.gibbs.sampler(documents=document,K=K,vocab=vocab,
+                                num.iterations=G,alpha=alpha,eta=eta,initial=NULL,
+                                burnin=0,compute.log.likelihood = TRUE)
+
+theta=t(apply(fit$document_sums+alpha,2,function(x) x/sum(x)))   #distribution of each songs in 20 topics
+phi=t(apply(t(fit$topics)+eta,2,function(x) x/sum(x)))    #distribution of each topics in 5000 word
